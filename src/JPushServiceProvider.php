80d33cc3de9574ca0f2262ba74a77e2ad7483a0a
hedeqiang <laravel_code@163.com>
2020-10-09 17:54:31 +0800

    Update src/JPushServiceProvider.php

--- src/JPushServiceProvider.php
+++ src/JPushServiceProvider.php
@@ -16,27 +16,27 @@ class JPushServiceProvider extends \Illuminate\Support\ServiceProvider
 
     public function register()
     {
-        $this->app->singleton('jpush.push', function () {
+        $this->app->singleton(JPush::class, function () {
             return new JPush(config('jpush'));
         });
-        $this->app->singleton('jpush.device', function () {
+        $this->app->singleton(Device::class, function () {
             return new Device(config('jpush'));
         });
-        $this->app->singleton('jpush.file', function () {
+        $this->app->singleton(File::class, function () {
             return new File(config('jpush'));
         });
-        $this->app->singleton('jpush.report', function () {
+        $this->app->singleton(Report::class, function () {
             return new Report(config('jpush'));
         });
-        $this->app->singleton('jpush.schedule', function () {
+        $this->app->singleton(Schedule::class, function () {
             return new Schedule(config('jpush'));
         });
 
-//        $this->app->alias(JPush::class, 'jpush.push');
-//        $this->app->alias(Device::class, 'jpush.device');
-//        $this->app->alias(File::class, 'jpush.file');
-//        $this->app->alias(Report::class, 'jpush.report');
-//        $this->app->alias(Schedule::class, 'jpush.schedule');
+        $this->app->alias(JPush::class, 'jpush.push');
+        $this->app->alias(Device::class, 'jpush.device');
+        $this->app->alias(File::class, 'jpush.file');
+        $this->app->alias(Report::class, 'jpush.report');
+        $this->app->alias(Schedule::class, 'jpush.schedule');
     }
 
     public function provides()
