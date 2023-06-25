e0906a95c1468fa601ff2be9d0ef8b7a654a79de
hedeqiang <laravel_code@163.com>
2023-06-25 10:40:57 +0800

    feat: Plan to support email.

---
+++ src/JEmail/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the hedeqiang/easyjiguang.
+ *
+ * (c) hedeqiang<laravel_code@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyJiGuang\JEmail;
+
+use EasyJiGuang\Kernel\Exceptions\InvalidConfigException;
+use EasyJiGuang\Kernel\Support\BaseClient;
+use GuzzleHttp\Exception\GuzzleException;
+
+class Client extends BaseClient
+{
+    const ENDPOINT_TEMPLATE = 'https://api.sendcloud.net/apiv2/';
+
+    public function send()
+    {
+        
+    }
+}
